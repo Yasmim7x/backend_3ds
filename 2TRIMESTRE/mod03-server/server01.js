91badeae6767620641d9f7e2c6e875269f8413bb
Yasmim7x <yasmim.lopes.silva@escola.pr.gov.br>
2026-08-04 11:51:07 +0000

    Atualiza páginas HTML e ajusta servidor para servir conteúdo dinâmico

--- 2TRIMESTRE/mod03-server/server01.js
+++ 2TRIMESTRE/mod03-server/server01.js
@@ -1,15 +1,31 @@
 const http = require('node:http')
-const porta = 8002
+const path = require('node:path')
+const fs = require('node:fs')
+const porta = 8081
+
+
+const home = path.join(__dirname, 'pages/index.html')
+const sobre = path.join(__dirname, 'pages/sobre.html')
+
 
 const server = http.createServer((req, res)=>{
     const novaUrl = new URL(req.url, `http://${req.headers.host}`) //http://localhost
     const caminhoUrl = novaUrl.pathname
+
     if(caminhoUrl === '/'){
 
-        res.statusCode = '201'
+        res.statusCode = '200'
         res.setHeader('Content-Type', 'text/html; charset=utf-8')
-        res.end('<h3>HTML Rocks!!</h3>')
-    } else { 
+        return res.end(fs.readFileSync(home, 'utf-8'))
+    } 
+    
+    if(caminhoUrl === '/sobre'){
+
+        res.statusCode = '200'
+        res.setHeader('Content-Type', 'text/html; charset=utf-8')
+        return res.end(fs.readFileSync(sobre, 'utf-8'))
+    } 
+    else { 
 
     res.statusCode = '401'
     res.setHeader('Content-Type', 'text/html; charset=utf-8')
